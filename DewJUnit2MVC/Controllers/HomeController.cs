6b0a21958b431c52c420011ce3ae695f639ed3cb
Jeffrey Dew <jeffreywdew@gmail.com>
2024-01-05 14:47:36 -0600

    Add project files.

---
+++ DewJUnit2MVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using DewJUnit2MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DewJUnit2MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Output(Customer model)
+        {
+            // checks if model state is valid, sets ViewBag properties accordingly
+            if (ModelState.IsValid)
+            {
+                // runs the customer model's method to create an email, if valid
+                ViewBag.CU = model.DetermineEmail();
+                ViewBag.Name = model.Name;
+                ViewBag.title = "Thank You";
+            }
+            else
+            {
+                ViewBag.CU = "N/A";
+                ViewBag.Name = "visitor";
+                ViewBag.title = "*extremely loud incorrect buzzer*";
+            }
+            return View(model);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DewJUnit2MVC/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DewJUnit2MVC.Models
+{
+    public class Customer
+    {
+        [Required(ErrorMessage = "Please enter a name")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Please enter a username")]
+        public string? Username { get; set; }
+        [Required(ErrorMessage = "Please enter a password")]
+        public string? Password { get; set; }
+
+        public string? DetermineEmail()
+        {
+            string? str = Username; //temp variable
+            // passes str through a filter to trash the special characters and build a new string with the allowed ones
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in str)
+            {
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_')
+                {
+                    sb.Append(c);
+                }
+            }
+            // returns processed string as an email address
+            return sb.ToString() + "@thisisfake.com";
+        }
+    }
+}
